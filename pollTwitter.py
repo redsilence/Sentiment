f05cb6ff570601d7db7c966a73a939de8e3dfb09
HarshUpadhyay <Harsh.Upadhyay@rice.edu>
2016-01-16 02:00:16 -0600

    creating twitterPoll

---
+++ pollTwitter.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, print_function
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+consumer_key = ""
+consumer_secret = ""
+
+
+access_token = ""
+access_token_secret = ""
+
+
+class TweetListener(StreamListener):
+    def on_data(self, data):
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    l = TweetListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=['basketball'])

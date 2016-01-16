0a3f048cf54c1a25bf608bab57d4152fd3629350
jagrawal36 <Jug83287>
2016-01-16 01:16:56 -0600

    test

--- getSentiment.py
+++ getSentiment.py
@@ -1,7 +1,7 @@
 # These code snippets use an open-source library.
 import unirest
 
-aa="i hate sam"
+aa="i hate sam "
 response = unirest.get("https://alchemy.p.mashape.com/text/TextGetTextSentiment?outputMode=json&showSourceText=false&text=i+hate+sam",
   headers={
     "X-Mashape-Key": "QudYSp65Jcmsh98Jm0wjHFrL5e3Xp1ALvSMjsn9f2DANpOb8aI",

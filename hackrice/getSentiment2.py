9928bf39626cb24b6f7ca08a5ed62f00a28de330
jagrawal36 <Jug83287>
2016-01-17 08:50:59 -0600

    hackrice

---
+++ hackrice/getSentiment2.py
@@ -0,0 +1,178 @@
+import nltk
+import numpy
+import re
+import numpy as np
+import argparse
+from operator import add
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--train", help="train")
+parser.add_argument("--tweet", help="tweet")
+args = parser.parse_args()
+
+
+def find_ones(a):
+    b=np.asarray(a)
+    c=np.where(b==1)
+    return(len(c[0]))
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+f = file_len(args.train)/2
+
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+      all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+tweets = []
+for i,line in enumerate(open(args.train)):
+    if i<f:
+    	words=re.split(' ',line)
+    	#print(words[0:len(words)-1])
+    	sent=words[-1].rstrip(' \n')
+    	#print(sent)
+    	wds = words[0:len(words)-1]
+    	#from IPython import embed
+    	#embed()
+    	f= [e.lower() for e in wds if len(e) >= 3]
+    	#print(f)
+    	#print('Done')
+    	tweets.append((f, sent))
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+n_pos=[]
+for line in open(args.tweet):
+    #print('Line is: ',line)
+    out = classifier.classify(extract_features(line.split()))
+    #print(out)
+    if out == "positive":
+        n_pos.append(1)
+    elif out == "negative":
+        n_pos.append(-1)
+    else:
+        n_pos.append(0)
+
+ff = file_len(args.tweet)
+print("Positives = ",n_pos)
+print("total nos= ",ff)
+print(find_ones(n_pos))
+
+
+
+
+# starting second round
+
+
+#del(training_set)
+#del(classifier)
+#del(word_features)
+f = file_len(args.train)/2
+
+#print(f)
+
+tweets = []
+for i,line in enumerate(open(args.train)):
+    #print(i,f)
+    if i>f:
+    	words=re.split(' ',line)
+    	#print(i,f,words[0:len(words)-1])
+    	sent=words[-1].rstrip(' \n')
+    	#print(sent)
+    	wds = words[0:len(words)-1]
+    	#from IPython import embed
+    	#embed()
+    	ff= [e.lower() for e in wds if len(e) >= 3]
+    	#print(f)
+    	#print('Done')
+    	tweets.append((ff, sent))
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+n1_pos=[]
+for line in open(args.tweet):
+    #print('Line is: ',line)
+    out = classifier.classify(extract_features(line.split()))
+    #print(out)
+    if out == "positive":
+        n1_pos.append(1)
+    elif out == "negative":
+        n1_pos.append(-1)
+    else:
+        n1_pos.append(0)
+
+ff = file_len(args.tweet)
+print("Positives = ",n1_pos)
+print("total nos= ",ff)
+print(find_ones(n1_pos))
+
+
+# starting third round
+
+f = file_len(args.train)/2
+
+#print(f)
+
+tweets = []
+for i,line in enumerate(open(args.train)):
+    #print(i,f)
+    words=re.split(' ',line)
+    #print(i,f,words[0:len(words)-1])
+    sent=words[-1].rstrip(' \n')
+    #print(sent)
+    wds = words[0:len(words)-1]
+    #from IPython import embed
+    #embed()
+    ff= [e.lower() for e in wds if len(e) >= 3]
+    #print(f)
+    #print('Done')
+    tweets.append((ff, sent))
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+n2_pos=[]
+for line in open(args.tweet):
+    #print('Line is: ',line)
+    out = classifier.classify(extract_features(line.split()))
+    #print(out)
+    if out == "positive":
+        n2_pos.append(1)
+    elif out == "negative":
+        n2_pos.append(-1)
+    else:
+        n2_pos.append(0)
+
+ff = file_len(args.tweet)
+print("Positives = ",n2_pos)
+print("total nos= ",ff)
+print(find_ones(n2_pos))
+
+answer=map(add,n_pos,n1_pos)
+answer=map(add,answer,n2_pos)
+an=np.asarray(answer)
+print(an)
+print("Mean=",an.mean())
+
---
+++ hackrice/pollTwitter.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, print_function
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+
+consumer_key = "rhUTo3IDEfktQcL3gz3qSM8og"
+consumer_secret = "ZlAKPOO1rl1objODHcwA45qIsSMxdqOux7FJsNXXbN2k1hoI5p"
+
+
+access_token = "304944155-u5BT348XsEpZgn7HldFS6WZBX2arnhPU1DxJt3It"
+access_token_secret = "kzM2i4YhRs2eunEmcoT8wdomQHp2l3VEwSc1WRAkkYqC3"
+
+track_string = 'iPhone 6S'
+
+
+class TweetListener(StreamListener):
+    def __init__(self, file_descriptor):
+        self.file_desc = file_descriptor
+
+    def on_data(self, data):
+        txt = json.loads(data)
+        if 'text' in txt:
+            txt = txt['text']
+        txt = txt.replace('\n', ' ').replace('\r', '')
+        self.file_desc.write((txt+"\n").encode("UTF-8"))
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    filedesc = open("./generated data/Raw_Tweets_iPhone6S.txt", 'w')
+    l = TweetListener(filedesc)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=[track_string])
